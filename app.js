efbe47ed0ab052aab8934f159e17add0a3c2d630
Patrick 1 <Patrick@Patricks-MacBook-Pro.local>
2017-12-03 22:59:31 -0800

    removed row and left basics

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 
-console.log("hello");
+console.log("js running");
 
 //let myHTML = `<h3>${project.title}</h3>`;
 // with more for the other pieces of data you have about your project
 $(document).ready(function(){
 
-
-
 let myProjects = [
 {
-name: "ProjectA", 
-image: "tba",
-link: "tba"
+name: "Project A", 
+image: "https://lh4.ggpht.com/bTBZkXZteZNIPw9dRdVzKyYpL8oo9R_ox3GyMcxm8vOCqJQQPk2Ne4NQFsMr49-f9b8=w300",
+link: "///Users/Patrick/Desktop/Practicing%20Self%20Study%20Codes/jQuery%20Self%20Study%20Code.html"
 },
 {
-name: "ProjectB", 
-image: "tba",
-link: "tba"
+name: "Project B", 
+image: "https://lh4.ggpht.com/bTBZkXZteZNIPw9dRdVzKyYpL8oo9R_ox3GyMcxm8vOCqJQQPk2Ne4NQFsMr49-f9b8=w300",
+link: "///Users/Patrick/Desktop/bootstraplab.html"
 },
 {
 name: "Memory Game", 
-image: "tba",
+image: "images/queen-of-hearts.png",
 link: "///Users/Patrick/fundamentals/wdi-fundamentals-memorygame2/index.html"
 }
 ];
