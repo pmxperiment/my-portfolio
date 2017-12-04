aa8b017b480e7d008566859a9233bcfaf2b47bca
Patrick 1 <Patrick@Patricks-MacBook-Pro.local>
2017-12-03 21:53:09 -0800

    changed hard code for jQuery loop

--- app.js
+++ app.js
@@ -1,55 +1,33 @@
 
 console.log("hello");
- console.log("test");
 
 //let myHTML = `<h3>${project.title}</h3>`;
 // with more for the other pieces of data you have about your project
+$(document).ready(function(){
 
 
 
-var projectsArray = [
+let myProjects = [
 {
-	type: 'WEBDEV',
-	projects: [
-		{past: 'Memory Game'},
-		{present: 'WDI Portfolio'},
-		{future: 'TBA'}
-	], 
+name: "ProjectA", 
+image: "tba",
+link: "tba"
 },
-{	type: 'VISUALS',
-	projects: [
-		{past: '2014-2016'},
-		{present: '2017'},
-		{future: 'TBA'}
-	], 
+{
+name: "ProjectB", 
+image: "tba",
+link: "tba"
 },
-{	type: 'MARKETING',
-	projects: [
-		{past: 'CalBox'},
-		{present: 'Hangnever'},
-		{future: 'TBA'}
-	] 
+{
+name: "Memory Game", 
+image: "tba",
+link: "///Users/Patrick/fundamentals/wdi-fundamentals-memorygame2/index.html"
 }
 ];
 
-//projectArray[0].projects[0]   the is the mem game
-
-
-
-Let products = [
-{
-Name: “Mem Game”, 
-Image: n/a,
-Link: n/a
-},
-{
-Name: “Security”, 
-Image: n/a
-Link: n/a
-},
-{
-Name: “Mem Game”, 
-Price: $15
-Link: "///Users/Patrick/fundamentals/wdi-fundamentals-memorygame2/index.html"
+for (var i=0; i<myProjects.length;i++) {
+	var projectText = "<a class='dropdown-item' href='#'>" + myProjects[i].name + '</a>';
+	$("#dropdown-menu").append(projectText);
 }
-];
+
+});
