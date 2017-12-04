98d331ec0dcbcd2ede395957071ebc032cefca19
Patrick 1 <Patrick@Patricks-MacBook-Pro.local>
2017-12-03 16:12:24 -0800

    created html css js files and arrays for projects

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-test
+
+console.log("hello");
+ console.log("test");
+
+//let myHTML = `<h3>${project.title}</h3>`;
+// with more for the other pieces of data you have about your project
+
+
+
+var projectsArray = [
+{
+	type: 'WEBDEV',
+	projects: [
+		{past: 'Memory Game'},
+		{present: 'WDI Portfolio'},
+		{future: 'TBA'}
+	], 
+},
+{	type: 'VISUALS',
+	projects: [
+		{past: '2014-2016'},
+		{present: '2017'},
+		{future: 'TBA'}
+	], 
+},
+{	type: 'MARKETING',
+	projects: [
+		{past: 'CalBox'},
+		{present: 'Hangnever'},
+		{future: 'TBA'}
+	] 
+}
+];
+
+//projectArray[0].projects[0]   the is the mem game
+
+
+
+Let products = [
+{
+Name: “Mem Game”, 
+Image: n/a,
+Link: n/a
+},
+{
+Name: “Security”, 
+Image: n/a
+Link: n/a
+},
+{
+Name: “Mem Game”, 
+Price: $15
+Link: "///Users/Patrick/fundamentals/wdi-fundamentals-memorygame2/index.html"
+}
+];
